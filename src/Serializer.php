d1747e0d424e179fb9349fa95ad031e1f0f7c346
Yannis Touili <gowtcha@gmail.com>
2017-02-28 09:48:21 +0100

    piece of serializer

--- src/Serializer.php
+++ src/Serializer.php
@@ -11,7 +11,7 @@ namespace FacturationPro;
 
 class Serializer
 {
-    public function serialize($object,$route)
+    /*public function serialize($object,$route)
     {
         $reflectionClass = new \ReflectionClass(get_class($object));
         $array = array();
@@ -21,6 +21,42 @@ class Serializer
                 $array[$property->getName()] = $property->getValue($object);
             $property->setAccessible(false);
         }
+        return $array;
+    }*/
+
+    public function serialize($element, $route)
+    {
+        $depth = 0;
+        if(is_array($element))
+        {
+            foreach($element as &$item)
+                $element = self::object($item,$depth, $route);
+        }
+        else
+            $element = self::object($element,$depth, $route);
+        return $element;
+    }
+
+    public function object($object,$depth, $route) {
+        if($depth > 2) return;
+        $reflectionClass = new \ReflectionClass(get_class($object));
+        $array = array();
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            $value = $property->getValue($object);
+            if(is_array($value))
+            {
+                foreach ($value as &$element) {
+                    $element = self::object($element,$depth + 1, $route);
+                }
+            }
+            elseif(is_object($value) && strpos(get_class($value),"FacturationPro") !== false)
+                $value = self::object($value,$depth + 1, $route);
+            if(!in_array($property->getName(),$route->getReadable()) && !is_array($property->getValue($object)) && $property->getValue($object))
+                $array[$property->getName()] = $value;
+        }
+        dump($object);
+        dump($array);
         return $array;
     }
 }
