7273f425b65a94bff0963f5dfd7c66f2e4b047ac
Gaetan Hautecoeur <gaetan.hautecoeur@gmail.com>
2019-04-18 10:12:38 +0200

    vat_exemption update API 2017 - https://support.facturation.pro/hc/fr/articles/115000006432-Modification-des-champs-d-exon%C3%A9ration-de-TVA

--- src/Entity/VatExemption.php
+++ src/Entity/VatExemption.php
@@ -28,18 +28,6 @@ class VatExemption
     CONST VAT_EXEMPTION_ART_293_B_DU_CGI = "art. 293 B du CGI";
     CONST VAT_EXEMPTION_AUTOLIQUIDATION = "Autoliquidation";
 
-    protected $list = array(
-        '20' => 'art. 259 A.2 du CGI',
-        '30' => 'art. 259 A.5 du CGI',
-        '40' => 'art. 259 B du CGI',
-        '10' => 'art. 259.1 du CGI',
-        '60' => 'art. 262 I du CGI',
-        '50' => 'art. 262 ter 1 du CGI',
-        '70' => 'art. 283.2 du CGI',
-        '100' => 'art. 293 B du CGI',
-        '1' => 'Autoliquidation'
-    );
-
     /** @var string $title */
     public function __construct($title)
     {
